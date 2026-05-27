42a50592f100e9672ac842d980b1808555df9780
SaiPreethi-12 <saipreethi.p@pragmr.com>
2026-05-27 12:21:08 +0530

    Variables, conditions, loop and functions added

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+func displayMessage(name string) {
+	fmt.Println("Hello", name)
+}
+
 func main() {
-	fmt.Println("Hello World")
+
+	// Variable
+	age := 20
+
+	// Condition
+	if age >= 18 {
+		fmt.Println("Eligible to vote")
+	} else {
+		fmt.Println("Not eligible to vote")
+	}
+
+	// Loop
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Number:", i)
+	}
+
+	// Function call
+	displayMessage("Go User")
 }
