0545caed670b2365ab5afb6ac16ddb1dbb451520
SaiPreethi-12 <saipreethi.p@pragmr.com>
2026-05-27 17:20:53 +0530

    Home page with MVC

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
-// package main
-
-// import (
-// 	"fmt"
-// 	"sync"
-// 	"time"
-// )
-
-// // Sleep sort function
-// func sleepSort(num int, wg *sync.WaitGroup) {
-// 	defer wg.Done()
-
-// 	// Sleep based on number value
-// 	time.Sleep(time.Duration(num) * time.Second)
-
-// 	fmt.Println(num)
-// }
-
-// func main() {
-
-// 	numbers := []int{3, 1, 4, 2}
-
-// 	var wg sync.WaitGroup
-
-// 	for _, num := range numbers {
-// 		wg.Add(1)
-
-// 		// Run goroutine
-// 		go sleepSort(num, &wg)
-// 	}
-
-// 	wg.Wait()
-
-//		fmt.Println("Sleep sort completed")
-//	}
 package main
 
-import "project/utils"
+import (
+	"project/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	utils.PrintMessage()
+
+	app := fiber.New()
+
+	routes.Setup(app)
+
+	app.Listen(":3000")
 }
