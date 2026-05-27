4a618fd730f83789a236ae1385d4d4c3a69eebcc
SaiPreethi-12 <saipreethi.p@pragmr.com>
2026-05-27 20:18:32 +0530

    Added redis cache for get requests

--- controllers/home.go
+++ controllers/home.go
@@ -1,9 +1,39 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+	"project/config"
+	"project/models"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func Home(c *fiber.Ctx) error {
-	return c.Render("user", fiber.Map{
-		"Name": "Kruthika",
+
+	cachedData, err := config.RedisClient.Get(config.Ctx, "users").Result()
+
+	if err == nil {
+
+		var cachedUsers []models.User
+
+		json.Unmarshal([]byte(cachedData), &cachedUsers)
+
+		return c.JSON(fiber.Map{
+			"source": "redis cache",
+			"data":   cachedUsers,
+		})
+	}
+
+	var users []models.User
+
+	config.DB.Find(&users)
+
+	jsonData, _ := json.Marshal(users)
+
+	config.RedisClient.Set(config.Ctx, "users", jsonData, 0)
+
+	return c.JSON(fiber.Map{
+		"source": "postgres database",
+		"data":   users,
 	})
 }
